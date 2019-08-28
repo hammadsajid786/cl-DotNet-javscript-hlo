ec8bb762459122d3dfca4fda87aef0dc573b2832
Hammad Sajid <786hammadsajid@gmail.com>
2019-08-28 23:52:51 +0500

    Clearing Brlmis from helo

--- BLRMIS Website/BLRMIS.Web.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BLRMIS.Web.Core
-{
-    public class Class1
-    {
-    }
-}
--- BLRMIS Website/BLRMIS.Web.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BLRMIS.Web.DataAccess
-{
-    public class Class1
-    {
-    }
-}
