4dc69f88aa91be645531e83660224f5c6420eaea
Hammad S <786hammadsajid@gmail.com>
2019-11-03 21:35:59 +0500

    Database Directory Reset
    Updated database

--- Databases/Tables/dbo.UserNames.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[UserNames]
-(
-[id] [int] NOT NULL IDENTITY(1, 1),
-[Name] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[UserNames] ADD CONSTRAINT [PK_UserNames] PRIMARY KEY CLUSTERED  ([id]) ON [PRIMARY]
-GO
