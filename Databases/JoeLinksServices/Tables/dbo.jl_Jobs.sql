e17107c40bac036e006291cca1f2ef993589618d
Hammad S <786hammadsajid@gmail.com>
2020-06-24 16:42:18 +0500

    jo_Job table added

---
+++ Databases/JoeLinksServices/Tables/dbo.jl_Jobs.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[jl_Jobs]
+(
+[JobId] [int] NOT NULL IDENTITY(1, 1),
+[CreatedbyUserId] [bigint] NOT NULL,
+[CreatedDateTime] [datetime] NOT NULL,
+[Title] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Location] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[JobFunctions] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[EmploymentType] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CompanyIndustries] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[SeniorityLevel] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[JobDescription] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Active] [bit] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[jl_Jobs] ADD CONSTRAINT [PK_jl_Jobs] PRIMARY KEY CLUSTERED  ([JobId]) ON [PRIMARY]
+GO
