e8ec75c80919fc9985c41de917c2aeb095c0ae64
Hammad Sajid <786hammadsajid@gmail.com>
2020-10-25 23:38:39 +0500

    Jl Jobs, jl _updatesEmails table Added

--- Databases/JoeLinksServices/Tables/dbo.jl_Jobs.sql
+++ Databases/JoeLinksServices/Tables/dbo.jl_Jobs.sql
@@ -2,18 +2,20 @@ CREATE TABLE [dbo].[jl_Jobs]
 (
 [JobId] [int] NOT NULL IDENTITY(1, 1),
 [PlaceId] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[CompanyName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CompanyName] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CreatedbyUserId] [bigint] NOT NULL,
 [CreatedDateTime] [datetime] NOT NULL,
-[Title] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Location] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Title] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Location] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [JobFunctions] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [EmploymentType] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [CompanyIndustries] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SeniorityLevel] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ResumeEmail] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [JobDescription] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[Active] [bit] NOT NULL
+[Active] [bit] NOT NULL,
+[ThirdPartyId] [int] NULL,
+[ThirdPartySource] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[jl_Jobs] ADD CONSTRAINT [PK_jl_Jobs] PRIMARY KEY CLUSTERED  ([JobId]) ON [PRIMARY]
